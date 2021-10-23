8a3fcc47000df199aefc160171227b9758dce311
jsungg <justinsungg@gmail.com>
2021-10-22 22:44:04 -0700

    finished website

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,11 @@
 import React from "react"
 import 'antd/dist/antd.css';
-import Navbar from "../components/navbar";
 import Page1 from "./landing/landing";
 
 
 export default function Home() {
   return (
     <>
-      <Navbar key="navbar" />
       <Page1 key="page1" />
     </>
   );
--- src/pages/landing/landing.jsx
+++ src/pages/landing/landing.jsx
@@ -1,17 +1,21 @@
-import React, { useEffect, useRef } from "react"
+import React, { useEffect, useRef, useState } from "react"
 import "./page1.less"
 import profilePic from "../../components/static/profile.png"
+import resume from "../../components/static/JSungResume2021.pdf"
 
 function Page1() {
   const canvasRef = useRef(null)
+  const [width, setWidth] = useState(window.innerWidth)
+  const [height, setHeight] = useState(window.innerHeight)
 
+  const updateWidthAndHeight = () => {
+    setWidth(window.innerWidth)
+    setHeight(window.innerHeight)
+  }
 
   useEffect(() => {
     const canvas = canvasRef.current
-    const context = canvas.getContext("2d");
-
-    canvas.width = window.innerWidth
-    canvas.height = window.innerHeight;
+    const context = canvas.getContext("2d")
 
     context.filter = "blur(00px)"
 
@@ -50,8 +54,17 @@ function Page1() {
 
     balls.push(new Circle(30, canvas.height, canvas.height / 2.5, "#82bfb5"))
     balls.push(new Circle(20, 20, canvas.height / 1.5, "#87e0d1"))
-    balls.push(new Circle(canvas.width / 1.4, canvas.height / 4, canvas.height / 3, "#73c7b9"))
-    balls.push(new Circle(canvas.width, canvas.height, canvas.height / 1.2, "#b4ede3"))
+    balls.push(
+      new Circle(
+        canvas.width / 1.4,
+        canvas.height / 4,
+        canvas.height / 3,
+        "#73c7b9"
+      )
+    )
+    balls.push(
+      new Circle(canvas.width, canvas.height, canvas.height / 1.2, "#b4ede3")
+    )
 
     function update() {
       context.clearRect(0, 0, canvas.width, canvas.height)
@@ -65,21 +78,56 @@ function Page1() {
     update()
   })
 
+  useEffect(() => {
+    window.addEventListener("resize", updateWidthAndHeight)
+    return () => window.removeEventListener("resize", updateWidthAndHeight)
+  })
+
   return (
-    <section>
-      <div className="wrapper">
-        <div className="row">
-          <div className="profile-pic">
-            <img src={profilePic} alt="Profile Pic of Justin Sung"/>
-          </div> 
-          <div className="text">
-            <h1>Hi, I'm Justin</h1>
-            <p>I am an incoming Senior at UC San Diego who is currently pursuing a Bachelor's Degree in Math-Computer Science and is expected to graduate by 2021. Currently I am looking for internships in Front-End Development for both mobile and web based applications. Feel free to check out my website :)</p>
+    <>
+      <header>
+        <div className="menu">
+          <h2 className="hover-underline-animation">
+            <a href={"https://github.com/jsungg"}>Projects</a>
+          </h2>
+
+          <h2 className="hover-underline-animation">
+            <a href={resume}>Resume</a>
+          </h2>
+        </div>
+      </header>
+
+      <section>
+        <div className="background">
+          <canvas
+            ref={canvasRef}
+            width={width}
+            height={height}
+            style={{
+              width: `${width}px`,
+              height: `${height}px`,
+            }}
+          />
+        </div>
+        <div className="container">
+          <div className="split">
+            <div className="profile-pic">
+              <img src={profilePic} alt="Profile Pic of Justin Sung" />
+            </div>
+            <div className="text">
+              <h1>Hi, I'm Justin</h1>
+              <p>
+                I am an incoming Senior at UC San Diego who is currently
+                pursuing a Bachelor's Degree in Math-Computer Science and is
+                expected to graduate by 2021. Currently I am looking for
+                internships in Front-End Development for both mobile and web
+                based applications. Feel free to check out my website :)
+              </p>
+            </div>
           </div>
         </div>
-     </div>
-      <canvas ref={canvasRef} />
-    </section>
+      </section>
+    </>
   )
 }
 
